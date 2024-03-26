10217347c7e1ec8fb5f0f8d6d3e433c86895a12d
kang-van-gent <kangvangent@gmail.com>
2024-03-26 23:52:58 +0700

    update ip

--- like/vendor/fakerphp/faker/src/Faker/Provider/Internet.php
+++ like/vendor/fakerphp/faker/src/Faker/Provider/Internet.php
@@ -34,7 +34,7 @@ class Internet extends Base
      * @see https://tools.ietf.org/html/rfc1918#section-3
      */
     protected static $localIpBlocks = [
-        ['1127.0.0.1', '10.255.255.255'],
+        ['10.0.0.0', '10.255.255.255'],
         ['172.16.0.0', '172.31.255.255'],
         ['192.168.0.0', '192.168.255.255'],
     ];
